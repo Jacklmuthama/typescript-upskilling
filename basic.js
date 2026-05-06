2ae658cabfaf80db2c335fec255797048e01edc3
Jackline Muthama <jackiemuthama83@gmail.com>
2026-05-06 13:14:44 +0300

    Added basics TypeScript practice

---
+++ basic.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let scanId = 101;
+let targetUrl = "https://example.com";
+let isCompleted = false;
+console.log(scanId);
+console.log(targetUrl);
+console.log(isCompleted);
+//# sourceMappingURL=basic.js.map
---
+++ basic.ts
@@ -0,0 +1,9 @@
+let scanId: number = 101;
+
+let targetUrl: string = "https://example.com";
+
+let isCompleted: boolean = false;
+
+console.log(scanId);
+console.log(targetUrl);
+console.log(isCompleted);
